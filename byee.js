5e689d2e4aa85dde941baad9fad1ddfb24ef30eb
Mahita2 <54814394+Mahita2@users.noreply.github.com>
2020-06-26 10:37:08 +0530

    Delete byee.js

--- byee.js
+++
@@ -1,8 +0,0 @@
-(function(window) {
-    var saybyee = new Object();
-    var byee = "Good Bye";
-    saybyee.speak = function speak(name) {
-        console.log(byee + " " + name);
-    };
-    window.saybyee = saybyee;
-})(window);
