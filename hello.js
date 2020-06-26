6e065ba9d483e7eb6646879765b837a57dfd543a
Mahita2 <54814394+Mahita2@users.noreply.github.com>
2020-06-26 10:36:54 +0530

    Delete hello.js

--- hello.js
+++
@@ -1,8 +0,0 @@
-(function(window) {
-    var sayhello= new Object();
-    var hello = "Hello";
-    helloSpeaker.speak = function(name) {
-        console.log(hello + " " + name);
-    };
-    window.sayhello = sayhello;
-})(window);
